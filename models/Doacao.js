622e4db608d01810f11b02ccb77d27a80f7d060b
GUSTAVO RODRIGUES PAULINO <gustavo.paulino10@etec.sp.gov.br>
2025-08-20 12:07:02 -0300

    VAmo trabaia

---
+++ models/Doacao.js
@@ -0,0 +1,39 @@
+import { DataTypes } from 'sequelize';
+
+export default (sequelize) => {
+    const doacao = sequelize.define('doacao', {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false
+        },
+        nome: { 
+            type: DataTypes.STRING,
+            allowNull: false 
+        },
+        email: { 
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        valor: { 
+            type: DataTypes.FLOAT,
+            allowNull: false 
+        },
+        linkPix: { 
+            type: DataTypes.STRING,
+            allowNull: false 
+        },
+        mensagem: {
+            type: DataTypes.STRING,
+            allowNull: false 
+        },
+
+        
+    }, {
+        tableName: 'doacoes',
+        timestamps: true
+    });
+
+    return doacao;
+};
