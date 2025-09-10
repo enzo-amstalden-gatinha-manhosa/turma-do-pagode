06273fa5458b890b326ab6a57b0735e3400396c8
TyrenK <bizinottoluca@gmail.com>
2025-09-10 11:15:56 -0300

    Update usuarioRoutes.js

--- Venturus/src/routes/usuarioRoutes.js
+++ Venturus/src/routes/usuarioRoutes.js
@@ -5,7 +5,7 @@ import { Usuario as UsuarioModel } from '../models/Usuario';
 usuario.use(express.json());
 
 
-let tutores = [
+const tutores = [
     {
         id,
         nome_completo,
@@ -25,40 +25,65 @@ let tutores = [
         }
 ];
 
-// Rota GET - Listar todas as terfas
 usuario.get('/tutores', (req, res) => {
     res.json(tutores);
 })
 
-//Rota POST - Criar uma nova tarefa
-usuario.post('/tutores', async (req, res) => {
-    const novoTutor = {
-        nome_completo, senha, email, cidade, estado, idade,
-        telefone, celular, cpf, endereco, bairro, cep, instagram, facebook
-    } = req.body;
-    
-        const resultado = await UsuarioModel.create({
+usuario.post('/usuario', async (req, res) => {
+    try{  
+
+        const {
+            nome_completo, senha, email, cidade, estado, idade,
+            telefone, instagram, facebook, celular, cpf, endereco, bairro, cep
+          } = req.body;
+
+        if (!nome_completo || !senha || !email || !cidade || !estado || !idade || !telefone) {
+            return res.status(400).json({ erro: "Todos os campos obrigatórios devem ser preenchidos corretamente." });
+        }
+        
+        const tutorExistente = await UsuarioModel.findOne({ where: { email } });
+        if (tutorExistente) {
+            return res.status(400).json({ erro: "Email preenchido já está sendo utilizado." });
+        }
+        
+        const novoTutor = await UsuarioModel.create({
             nome_completo, senha, email, cidade, estado, idade,
             telefone, celular, cpf, endereco, bairro, cep, instagram, facebook
         });
-
-        console.log(resultado);
-
-    if( !nome_completo || !senha || !email || !cidade || !estado || !idade || !telefone) {
-        return tutores.push(novoTutor);
-    } else {
-        return res.status(400).json({erro: "Todos os campos obrigatórios devem ser preenchidos corretamente"});
+    } catch (error) {
+        return res.status(500).json({error: "Erro ao cadastrar usuário."})
     }
-        
     res.status(201).json(novoTutor);
 });
-
-//Rota PUT - Atualizar uma tarefa existente
-usuario.patch('/tutores/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const tutor = tutor.find((t) => t.id == id);
-    if (!tutor) return res.status(404).json({ error: 'Tutor não encontrada' });
-
-    tutor.nome = req.body.nome;
-    res.json(tutor);
+    usuario.patch('/tutores/:id', async (req, res) => {
+        try {
+            const id = req.params.id;
+    
+            const tutor = await UsuarioModel.findByPk(id);
+            if (!tutor) {
+                return res.status(404).json({ error: 'Tutor não encontrado' });
+            }
+    
+            const camposPermitidos = ['email', 'nome_completo', 'cidade', 'estado', 'questionario'];
+            let houveAtualizacao = false;
+    
+            camposPermitidos.forEach(campo => {
+                if (req.body[campo] !== undefined) {
+                    tutor[campo] = req.body[campo];
+                    houveAtualizacao = true;
+                }
+            });
+    
+            if (!houveAtualizacao) {
+                return res.status(400).json({ error: 'Pelo menos um campo deve ser mandado para atualização.' });
+            }
+    
+            await tutor.save();
+            res.json(tutor);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: "Erro ao atualizar os dados do tutor." });
+        }
 });
+    
+
