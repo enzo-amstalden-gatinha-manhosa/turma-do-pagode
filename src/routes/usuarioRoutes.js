1f78b7a52d196c8f7663383a5e82fecd9df61ee0
TyrenK <bizinottoluca@gmail.com>
2025-09-29 09:39:39 -0300

    Projeto

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,89 +1,13 @@
 const express = require('express');
-const usuario = express();
-import { Usuario as UsuarioModel } from '../models/Usuario';
+const router = express.Router();
+const UsuarioController = require('../controllers/UsuarioController');
 
-usuario.use(express.json());
+router.get('/:id', UsuarioController.listar);
 
+router.post('/', UsuarioController.criar);
 
-const tutores = [
-    {
-        id,
-        nome_completo,
-        senha,
-        email,
-        cidade,
-        estado,
-        idade,
-        telefone,
-        celular,
-        cpf,
-        endereco,
-        bairro,
-        cep,
-        instagram,
-        facebook
-        }
-];
+router.patch('/:id', UsuarioController.atualizar);
 
-usuario.get('/tutores', (req, res) => {
-    res.json(tutores);
-})
+module.exports = router;
 
-usuario.post('/usuario', async (req, res) => {
-    try{  
-
-        const {
-            nome_completo, senha, email, cidade, estado, idade,
-            telefone, instagram, facebook, celular, cpf, endereco, bairro, cep
-          } = req.body;
-
-        if (!nome_completo || !senha || !email || !cidade || !estado || !idade || !telefone) {
-            return res.status(400).json({ erro: "Todos os campos obrigatórios devem ser preenchidos corretamente." });
-        }
-        
-        const tutorExistente = await UsuarioModel.findOne({ where: { email } });
-        if (tutorExistente) {
-            return res.status(400).json({ erro: "Email preenchido já está sendo utilizado." });
-        }
-        
-        const novoTutor = await UsuarioModel.create({
-            nome_completo, senha, email, cidade, estado, idade,
-            telefone, celular, cpf, endereco, bairro, cep, instagram, facebook
-        });
-    } catch (error) {
-        return res.status(500).json({error: "Erro ao cadastrar usuário."})
-    }
-    res.status(201).json(novoTutor);
-});
-    usuario.patch('/tutores/:id', async (req, res) => {
-        try {
-            const id = req.params.id;
-    
-            const tutor = await UsuarioModel.findByPk(id);
-            if (!tutor) {
-                return res.status(404).json({ error: 'Tutor não encontrado' });
-            }
-    
-            const camposPermitidos = ['email', 'nome_completo', 'cidade', 'estado', 'questionario'];
-            let houveAtualizacao = false;
-    
-            camposPermitidos.forEach(campo => {
-                if (req.body[campo] !== undefined) {
-                    tutor[campo] = req.body[campo];
-                    houveAtualizacao = true;
-                }
-            });
-    
-            if (!houveAtualizacao) {
-                return res.status(400).json({ error: 'Pelo menos um campo deve ser mandado para atualização.' });
-            }
-    
-            await tutor.save();
-            res.json(tutor);
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: "Erro ao atualizar os dados do tutor." });
-        }
-});
-    
 
