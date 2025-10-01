9a546f25816ae6c47f6e56eb0c04ab77dba61b15
TyrenK <bizinottoluca@gmail.com>
2025-10-01 11:18:22 -0300

    Projeto

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const UsuarioController = require('../controllers/UsuarioController');
+const QuestionarioController = require('../controllers/QuestionarioController');
 
 router.get('/:id', UsuarioController.listar);
 
 router.post('/', UsuarioController.criar);
 
 router.patch('/:id', UsuarioController.atualizar);
+
+router.post('/questionario', QuestionarioController.criar);
 
 module.exports = router;
 
