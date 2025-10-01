8ff4221dd2acb31c6ad324872fd5d07b08a723be
Gabriel Rodrigues <gabriel.silva3804@etec.sp.gov.br>
2025-10-01 11:43:14 -0300

    Projeto

--- src/routes/animalRoutes.js
+++ src/routes/animalRoutes.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const animalController = require('../controllers/AnimalController');
+const multer = require('multer'); // Importa o multer para upload da foto... fazer upload dps
+
 
 router.get('/', animalController.listar);
 
