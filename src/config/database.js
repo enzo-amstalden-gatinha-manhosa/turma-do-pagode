90d47ed0e754eedbc992aca7c4f7de868e83dff3
TyrenK <bizinottoluca@gmail.com>
2025-10-06 16:28:06 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,16 @@
 const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const sequelize = new Sequelize({
   username: 'postgres',
   password: 'TurmaDoPagode1',
   database: 'postgres',
-  host: 'db.xpewwnsmtjvkbcywghsd.supabase.co',
+  host: 'db.yypmbdzhacvpbeymfcdn.supabase.co',
   port: 5432,
-  dialect: 'sqlite',
-  storage: './database.sqlite',
+  dialect: 'postgres',
   logging: false
 });
 
 module.exports = sequelize;
+
