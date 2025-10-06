50285d6a1106ece0488d2c87fa490d8a94b1dc00
enzo-amstalden-gatinha-manhosa <13enzomeida@gmail.com>
2025-10-05 21:23:17 -0300

    Novo Middleware que falta testar

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,25 +1,29 @@
-const { Usuario } = require('../models/Usuario');
+import jwt from 'jsonwebtoken';
+import sequelize from '../config/database.js';
+import UsuarioModel from '../models/Usuario.js';
+
+const Usuario = UsuarioModel(sequelize);
+const jwtSecret = "senhalegal";
 
 async function authMiddleware(req, res, next) {
+  const authHeader = req.get('Authorization');
+  if (!authHeader) return res.status(401).json({ erro: 'Token não fornecido.' });
+
+  const token = authHeader.split(' ')[1];
+  if (!token) return res.status(401).json({ erro: 'Formato de token inválido.' });
+
   try {
-    const cpf = req.headers['x-user-cpf']; 
-
-    if (!cpf) {
-      return res.status(401).json({ erro: 'Usuário não autenticado: header x-user-cpf ausente' });
-    }
-    
-    const usuario = await Usuario.findOne({ where: { cpf } });
-
-    if (!usuario) {
-      return res.status(401).json({ erro: 'Usuário não encontrado' });
-    }
+    const decoded = jwt.verify(token, jwtSecret);
+    const usuario = await Usuario.findByPk(decoded.id);
+    if (!usuario) return res.status(401).json({ erro: 'Usuário não encontrado.' });
 
     req.user = usuario;
     next();
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ erro: 'Erro interno na autenticação' });
+    console.error('Erro no token:', err);
+    const msg = err.name === 'TokenExpiredError' ? 'Token expirado. Faça login novamente.' : 'Token inválido.';
+    res.status(401).json({ erro: msg });
   }
 }
 
-module.exports = authMiddleware;
+export default authMiddleware;
